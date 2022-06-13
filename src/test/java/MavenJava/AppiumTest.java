b313fa171c29fae8202cc56f60651ae7566d4834
Navnidhi <navnidhisharma13@gmail.com>
2022-06-13 12:44:04 +0530

    first commit

---
+++ src/test/java/MavenJava/AppiumTest.java
@@ -0,0 +1,16 @@
+package MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+    @Test
+    public void androidApps() {
+        System.out.println("androidApps");
+    }
+
+    @Test
+    public void IOSApps() {
+        System.out.println("iOSApps");
+    }
+}
